5fa7d42f3263f0e95bfe67112382aedf40a5cb23
Kinga <kinga.kimbrel@gmail.com>
2016-10-30 17:22:09 -0500

    ready to push to Heroku

--- LearningLog/settings.py
+++ LearningLog/settings.py
@@ -126,3 +126,24 @@ LOGIN_URL = '/users/login/'
 BOOTSTRAP3 = {
     'include_jquery': True
 }
+
+# Heroku settings
+if __name__ == '__main__':
+    if __name__ == '__main__':
+        if os.getcwd() == 'app':
+            import dj_database_url
+
+            DATABASES = {
+                'default': dj_database_url.config(default='postgres://localhost')
+            }
+
+            # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+            SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+            # Allow all host headers
+            ALLOWED_HOSTS = ['*']
+
+            # Static assests configuration
+            BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+            STATIC_ROOT = 'staticfiles'
+            STATIC_FILES_DIRS = (os.path.join(BASE_DIR, 'static'))
