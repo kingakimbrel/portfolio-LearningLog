3c3e3828064287dcb78e6ec040203d68f716632a
Kinga <kinga.kimbrel@gmail.com>
2016-10-30 17:33:06 -0500

    fix on settings

--- LearningLog/settings.py
+++ LearningLog/settings.py
@@ -128,22 +128,20 @@ BOOTSTRAP3 = {
 }
 
 # Heroku settings
-if __name__ == '__main__':
-    if __name__ == '__main__':
-        if os.getcwd() == 'app':
-            import dj_database_url
+if os.getcwd() == 'app':
+    import dj_database_url
 
-            DATABASES = {
-                'default': dj_database_url.config(default='postgres://localhost')
-            }
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
 
-            # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-            SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-            # Allow all host headers
-            ALLOWED_HOSTS = ['*']
+    # Allow all host headers
+    ALLOWED_HOSTS = ['*']
 
-            # Static assests configuration
-            BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-            STATIC_ROOT = 'staticfiles'
-            STATIC_FILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+    # Static assests configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATIC_FILES_DIRS = (os.path.join(BASE_DIR, 'static'))
